e5819cdfd187e4153f0ee0ee33f07955fdc8a93f
Vickouma77 <Vickprestone20@gmail.com>
2024-09-01 12:01:13 +0300

    refactor(core): project restrucure

--- luminara-core/settings.gradle.kts
+++ luminara-core/settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "luminara-core"
+
+include("core-libs")
--- luminara-core/src/test/kotlin/com/luminara/luminara_core/TestLuminaraCoreApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.luminara.luminara_core
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<LuminaraCoreApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
